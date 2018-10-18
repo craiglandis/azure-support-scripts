d79b557b0d1871bd7f3e6c96e8d6a53259d05359
Craig Landis <craiglandis@users.noreply.github.com>
2018-10-17 23:31:07 -0700

    asdf

--- VMRecovery/ResourceManager/Enable-NestedHyperV.ps1
+++ VMRecovery/ResourceManager/Enable-NestedHyperV.ps1
@@ -5,10 +5,12 @@ start $env:windir\System32\mmc.exe $env:windir\System32\virtmgmt.msc
 start $env:windir\System32\vmconnect.exe localhost $nestedGuestVmName
 "@
 
-$features = get-windowsfeature
+$features = get-windowsfeature -ErrorAction Stop
 $hyperv = $features | where Name -eq 'Hyper-V'
 $hypervTools = $features | where Name -eq 'Hyper-V-Tools'
 $hypervPowerShell = $features | where Name -eq 'Hyper-V-Powershell'
+$dhcp = $features | where Name -eq 'DHCP'
+$rsatDhcp = $features | where Name -eq 'RSAT-DHCP'
 
 if ($hyperv.Installed -and $hypervTools.Installed -and $hypervPowerShell.Installed)
 {
@@ -17,44 +19,53 @@ if ($hyperv.Installed -and $hypervTools.Installed -and $hypervPowerShell.Install
     $return = New-ItemProperty -Path HKLM:\Software\Microsoft\ServerManager\Oobe -Name DoNotOpenInitialConfigurationTasksAtLogon -PropertyType DWORD -Value 1 -force -ErrorAction SilentlyContinue
 
     try {
+
+        # Configure NAT, see also https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/user-guide/nested-virtualization#networking-options
         $switch = get-vmswitch -Name Internal -SwitchType Internal -ErrorAction SilentlyContinue | select -first 1
         if (!$switch)
         {
             $switch = New-VMSwitch -Name Internal -SwitchType Internal -ErrorAction Stop
         }
-
         $adapter = Get-NetAdapter -Name 'vEthernet (Internal)' -ErrorAction Stop
 
         $ip = get-netipaddress -IPAddress 192.168.0.1 -ErrorAction SilentlyContinue | select -first 1
         if (!$ip)
         {
-            $ip = New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex $adapter.ifIndex -ErrorAction Stop
+            $return = New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex $adapter.ifIndex -ErrorAction Stop
         }
 
         $nat = Get-NetNat -Name InternalNAT -ErrorAction SilentlyContinue | select -first 1
         if (!$nat)
         {
-            $nat = New-NetNat -Name InternalNAT -InternalIPInterfaceAddressPrefix 192.168.0.0/24 -ErrorAction Stop
+            $return = New-NetNat -Name InternalNAT -InternalIPInterfaceAddressPrefix 192.168.0.0/24 -ErrorAction Stop
         }
 
-        $dhcp = Install-WindowsFeature -Name DHCP -IncludeManagementTools -ErrorAction Stop
-
-        $scope = Get-DhcpServerv4Scope | where Name -eq Scope1 | select -first 1
+        # Configure DHCP server service so nested guest can get an IP from DHCP and will use 168.63.129.16 for DNS and 192.168.0.1 as default gateway
+        if ($dhcp.Installed -eq $false -or $rsatDhcp.Installed -eq $false)
+        {
+            $return = Install-WindowsFeature -Name DHCP -IncludeManagementTools -ErrorAction Stop
+        }
+        $scope = Get-DhcpServerv4Scope -ErrorAction SilentlyContinue | where Name -eq Scope1 | select -first 1
         if (!$scope)
         {
-            $scope = Add-DhcpServerV4Scope -Name Scope1 -StartRange 192.168.0.100 -EndRange 192.168.0.200 -SubnetMask 255.255.255.0 -ErrorAction Stop
+            $return = Add-DhcpServerV4Scope -Name Scope1 -StartRange 192.168.0.100 -EndRange 192.168.0.200 -SubnetMask 255.255.255.0 -ErrorAction Stop
         }
-        $option = Set-DhcpServerV4OptionValue -Router 192.168.0.1 -ErrorAction Stop
-        $newvm = new-vm -name $nestedGuestVmName -MemoryStartupBytes 4GB -NoVHD -BootDevice IDE -Generation 1 -ErrorAction Stop
-        $setvm = set-vm -name $nestedGuestVmName -ProcessorCount 2 -CheckpointType Disabled -ErrorAction Stop
+        $return = Set-DhcpServerv4OptionValue -DnsServer 168.63.129.16 -Router 192.168.0.1 -ErrorAction Stop
+
+        # Create the nested guest VM
+        $return = new-vm -name $nestedGuestVmName -MemoryStartupBytes 4GB -NoVHD -BootDevice IDE -Generation 1 -ErrorAction Stop
+        $return = set-vm -name $nestedGuestVmName -ProcessorCount 2 -CheckpointType Disabled -ErrorAction Stop
         $disk = get-disk -ErrorAction Stop | where {$_.FriendlyName -eq 'Msft Virtual Disk'}
-        $disk | set-disk -IsOffline $true -ErrorAction Stop
-        $disk | Add-VMHardDiskDrive -VMName $nestedGuestVmName -ErrorAction Stop
-        $switch | Connect-VMNetworkAdapter -VMName $nestedGuestVmName
-        $startvm = start-vm -Name $nestedGuestVmName -ErrorAction Stop
+        $return = $disk | set-disk -IsOffline $true -ErrorAction Stop
+        $return = $disk | Add-VMHardDiskDrive -VMName $nestedGuestVmName -ErrorAction Stop
+        $return = $switch | Connect-VMNetworkAdapter -VMName $nestedGuestVmName -ErrorAction Stop
+        $return = start-vm -Name $nestedGuestVmName -ErrorAction Stop
         $nestedGuestVmState = (get-vm -Name $nestedGuestVmName -ErrorAction Stop).State
-        $batchFileContents | out-file -FilePath $batchFile -Force -Encoding Default
+
+        # Create a batch file in the all users startup folder so both Hyper-V Manager and VMConnect run automatically at logon.
+        $return = $batchFileContents | out-file -FilePath $batchFile -Force -Encoding Default
         $return = copy-item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Hyper-V Manager.lnk" -Destination "C:\Users\Public\Desktop"
+        # Suppress the prompt for "Do you want to allow your PC to be discoverable by other PCs and devices on this network"
         $return = new-item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
     }
     catch {
@@ -62,16 +73,18 @@ if ($hyperv.Installed -and $hypervTools.Installed -and $hypervPowerShell.Install
         exit 1
     }
 
+    # Returns the nested guest VM status to the calling script - "Running" if all went well.
     $nestedGuestVmState
 }
 else
 {
     try {
-        $result = install-windowsfeature -name Hyper-V -IncludeManagementTools -ErrorAction Stop
+        # Install Hyper-V role. The required restart is handled in the calling script, not this script, to make sure that this script cleanly returns the Hyper-V role install status to the calling script.
+        $return = install-windowsfeature -name Hyper-V -IncludeManagementTools -ErrorAction Stop
     }
     catch {
         throw $_
         exit 1
     }
-    write-host $result.ExitCode
+    write-host $return.ExitCode
 }
